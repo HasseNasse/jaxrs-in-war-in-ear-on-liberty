0cdf2a066f1ceaf18fbebe6a62f9c40b6bd00ca4
Hassan Nazar <hnazar@redbridge.se>
2018-11-14 00:04:06 +0100

    first commit

---
+++ jaxrs-in-war-2/src/main/java/io/codelair/trial/AnotherJaxRsApplication.java
@@ -0,0 +1,8 @@
+package io.codelair.trial;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class AnotherJaxRsApplication extends Application {
+}
---
+++ jaxrs-in-war/src/main/java/io/codelair/trial/greeting/GreetingResource.java
@@ -0,0 +1,17 @@
+package io.codelair.trial.greeting;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("greeting")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getGreeting(){
+        return Response.ok("Hello World").build();
+    }
+}
